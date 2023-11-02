94812960dec296bbc8c46158d1d1309605f6b20f
Angie Guia <angieguia@csu.fullerton.edu>
2023-11-01 19:42:02 -0700

    finished

--- part-2/average.cc
+++ part-2/average.cc
@@ -8,18 +8,18 @@
 #include <vector>
 
 int main(int argc, char* argv[]) {
- std::vector<std::string> arguments{argv, argv + argc};
- if (arguments.size() < 1) {
-   std::cout << "error: you must supply at least one number\n";
-   return 1;
- }
+  std::vector<std::string> arguments{argv, argv + argc};
+  if (arguments.size() < 1) {
+    std::cout << "error: you must supply at least one number\n";
+    return 1;
+  }
 
-double sum{0.0};
-for (int i = 1; i <= arguments.size()-1; ++i) {
-   sum += std::stod(arguments.at(i));
+  double sum{0.0};
+  for (int i = 1; i <= arguments.size() - 1; ++i) {
+    sum += std::stod(arguments.at(i));
+  }
+
+  double average = sum / (arguments.size() - 1);
+  std::cout << "average = " << average << "\n";
+  return average;
 }
-
-double average = sum / (arguments.size()-1);
- std::cout << "average = " << average << "\n";
-   return average;
- }
